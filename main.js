74b4fe31bdd41325ee2300845e6ac29c914b44b1
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2023-06-13 15:19:36 +0600

    problem 15 done

--- main.js
+++ main.js
@@ -145,7 +145,35 @@ const lengthToBytes = (str) => {
   console.log(lengthToBytes("false")) // 5
   console.log(lengthToBytes("12345")) // 5
 
-  //////////////////////////
-  /////////////////////
 
+//   problem 15. Write a JavaScript program to return the minimum-maximum value
+//  of an array, after applying the provided function to set a comparing rule. 
+
+const reduce_Which = (arr, comparator = (a, b) => a - b) =>
+
+  arr.reduce((a, b) => (comparator(a, b) >= 0 ? b : a));
+
+console.log(reduce_Which([1, 3, 2])); 
+
+console.log(reduce_Which([10, 30, 20], (a, b) => b - a));  
+
+console.log(reduce_Which(
+  [{ name: 'Kevin', age: 16 }, { name: 'John', age: 20 }, { name: 'Ani', age: 19 }],
+  (a, b) => a.age - b.age)); 
+
+/////////////////////////
+////////////
+
+function minMax(array, comparator) {
+    if (array.length === 0) {
+      return undefined;
+    }
   
+    const sortedArray = array.sort(comparator);
+    return sortedArray[0];
+  }
+  
+  const numbers = [10, 30, 20];
+  const comparator = (a, b) => a - b;
+  console.log(minMax(numbers, comparator));
+  console.log(minMax([{ name: 'Kevin', age: 16 }, { name: 'John', age: 20 }, { name: 'Ani', age: 19 }], comparator));
