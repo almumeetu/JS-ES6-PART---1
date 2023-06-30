675c69e5554a099cd73738d1d34d0a5feac5270b
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2023-06-30 21:27:20 +0600

    problem 67 done

--- main.js
+++ main.js
@@ -758,3 +758,13 @@ const multiplyAndAdd5 = compose(
   multiply
 );
 console.log(multiplyAndAdd5(5, 2));  /// 15
+
+// problem 67. Write a JavaScript program to perform left-to-right function composition.
+
+const composeRight = (...fns) => fns.reduce((f, g) => (...args) => g(f(...args)));
+const add = (x, y) => x + y;
+const square = x => x * x;
+const addAndSquare = composeRight(add, square);
+
+console.log(addAndSquare(1, 2));
+console.log(addAndSquare(3, 2));
