ea82d6f7913072050fb29b4f2e67df05264f6792
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2023-07-06 14:15:12 +0600

    problem 78 done

--- main.js
+++ main.js
@@ -895,3 +895,18 @@ forEachRight([1, 2, 3, 4], val => console.log(val)); //
 
 const forOwn = (obj, fn) => Object.keys(obj).forEach(key => fn(obj[key], key, obj));
 forOwn({ foo: 'bar', a: 1 }, v => console.log(v)); // 'bar', 1
+
+// problem 78. Write a JavaScript program to invert the key-value pairs of an object, 
+// without mutating it. The corresponding inverted value of
+//  each inverted key is an array of keys responsible for generating the inverted value. 
+//  If a function is supplied, it is applied to each inverted key.
+
+const invertKeyValues = (obj, fn) =>
+  Object.keys(obj).reduce((acc, key) => {
+    const val = fn ? fn(obj[key]) : obj[key];
+    acc[val] = acc[val] || [];
+    acc[val].push(key);
+    return acc;
+  }, {});
+console.log(invertKeyValues({ a: 1, b: 2, c: 1 })); // {"1":["a","c"],"2":["b"]}
+console.log(invertKeyValues({ a: 1, b: 2, c: 1 }, value => 'group' + value)); // {"group1":["a","c"],"group2":["b"]}
