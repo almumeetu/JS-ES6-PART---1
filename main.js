b670a5b69cee13362dffb13a777e962cf753673d
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2023-07-09 16:13:23 +0600

    problem 79 done

--- main.js
+++ main.js
@@ -910,3 +910,12 @@ const invertKeyValues = (obj, fn) =>
   }, {});
 console.log(invertKeyValues({ a: 1, b: 2, c: 1 })); // {"1":["a","c"],"2":["b"]}
 console.log(invertKeyValues({ a: 1, b: 2, c: 1 }, value => 'group' + value)); // {"group1":["a","c"],"group2":["b"]}
+
+// problem 79. Write a JavaScript program to take any number of iterable objects or objects with a length property and return the longest one.
+const longestItem = (...vals) => [...vals].sort((a, b) => b.length - a.length)[0];
+console.log(longestItem('this', 'is', 'a', 'testcase'));
+console.log(longestItem(...['a', 'ab', 'abc']));
+console.log(longestItem(...['a', 'ab', 'abc'], 'abcd'));
+console.log(longestItem([1, 2, 3], [1, 2], [1, 2, 3, 4, 5]));
+console.log(longestItem([1, 2, 3], 'foobar'));
+
