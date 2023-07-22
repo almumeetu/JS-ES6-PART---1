9b8849fe136419c6ef6966e3150a421b04ce9f31
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2023-07-22 19:37:44 +0600

    problem 82 done

--- main.js
+++ main.js
@@ -957,5 +957,12 @@ const mapKeys = (obj, fn) =>
   }, {});
 console.log(mapKeys({ a: 1, b: 2 }, (val, key) => key + val));
 
+//  problem 82: Write a JavaScript program to map array values to an object using a function. The key-value pairs consist of the original value as the key and the mapped value.
 
 
+const mapObject = (arr, fn) =>
+  (a => (
+    (a = [arr, arr.map(fn)]), a[0].reduce((acc, val, ind) => ((acc[val] = a[1][ind]), acc), {})
+  ))();
+const squareIt = arr => mapObject(arr, a => a * a);
+console.log(squareIt([1, 2, 3])); 
